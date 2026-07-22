0bcc6a77303b09c90af48337280fd5c6452a93d3
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2026-07-22 14:34:26 +0300

    add standart comparators

--- src/main/java/org/top/Main.java
+++ src/main/java/org/top/Main.java
@@ -1,22 +1,33 @@
 package org.top;
 
 import org.top.builder.CarBuilder;
+import org.top.comparator.CarComparator;
 import org.top.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Toyota", "Camry", 2020, "Red", 200, 30000, false));
+        cars.add(new Car("BMW", "X5", 2022, "Black", 250, 45000, true));
+        cars.add(new Car("Audi", "A4", 2021, "White", 190, 35000, false));
+        cars.add(new Car("Honda", "Civic", 2019, "Blue", 180, 25000, true));
+        cars.add(new Car("Toyota", "Corolla", 2021, "Silver", 140, 22000, true));
+        cars.add(new Car("BMW", "X3", 2021, "Black", 180, 40000, false));
+        cars.add(new Car("Mercedes", "E-Class", 2018, "White", 220, 50000, false));
+        cars.add(new Car("Audi", "Q7", 2022, "Black", 280, 55000, true));
 
-        Car car1 = new CarBuilder()
-                .setBrand("BMW")
-                .setModel("S300")
-                .setColor("white")
-                .setIsNew(true)
-                .setPower(120)
-                .setPrice(12000000.00)
-                .setYear(2026)
-                .build();
-        System.out.println(car1);
+        System.out.println("___Исходный список___");
+        cars.forEach(System.out::println);
+
+        System.out.println("___Отсортированный список___");
+        cars.stream()
+                .filter(car -> !car.isNew())
+                .sorted(CarComparator.BY_YEAR.reversed())//можно прям в стриме вызывать, если нужен другой порядок
+                .forEach(System.out::println);
     }
 }
---
+++ src/main/java/org/top/comparator/CarComparator.java
@@ -0,0 +1,28 @@
+package org.top.comparator;
+
+import org.top.model.Car;
+
+import java.util.Comparator;
+
+public class CarComparator {
+    public static final Comparator<Car> BY_BRAND =
+            Comparator.comparing(Car::getBrand);
+
+    public static final Comparator<Car> BY_MODEL =
+            Comparator.comparing(Car::getModel);
+
+    public static final Comparator<Car> BY_YEAR =
+            Comparator.comparingInt(Car::getYear);
+
+    public static final Comparator<Car> BY_COLOR =
+            Comparator.comparing(Car::getColor);
+
+    public static final Comparator<Car> BY_POWER =
+            Comparator.comparingInt(Car::getPower);
+
+    public static final Comparator<Car> BY_PRICE =
+            Comparator.comparingDouble(Car::getPrice);
+
+    public static final Comparator<Car> BY_IS_NEW =
+            Comparator.comparing(Car::isNew);//б/у в начале
+}
